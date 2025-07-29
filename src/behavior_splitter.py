9cb6322b7debbf9584c31d2adb0d6fbd233e30dc
emirseners <senerahmetemir@gmail.com>
2025-07-29 12:43:16 +0300

    distance and behavior splitter code changes

--- src/behavior_splitter.py
+++ src/behavior_splitter.py
@@ -2,139 +2,77 @@
 Created on Dec 16, 2014
 Updated 23
 @author: gonenc
+
+Updated July 25, 2025
 '''
 import numpy as np
-from sklearn import datasets, linear_model
 
 def construct_features(data, significanceLevel=0.01):
     '''
-    Constructs a feature vector for each of the data-series contained in the 
-    data. 
-    
+    Constructs a feature vector for each of the data-series contained in the data.
+
+    data: 2D array or np array; [[features1], [features2], ...]
+    return: 3D np array; [[[signSlopeFeature1Section1, signCurvatureFeature1Section1], [signSlopeFeature1Section2, signCurvatureFeature1Section2], ...], ...]
     '''
-    # TODO, the casting of each feature to a list of tuples might be 
-    # removed at some stage, it will lead to a speed up, for you 
-    # can vectorize the calculations that use the feature vector
+    if not isinstance(data, np.ndarray):
+        data = np.array(data)
+
     features = []
-    for i in range(len(data)):
-        feature = split_behavior(data[i])
+
+    slopes = np.gradient(data, axis=1)
+    curvatures = np.gradient(slopes, axis=1)
+
+    for i in range(data.shape[0]):
+        feature = split_behavior(data[i], slopes[i], curvatures[i], significanceLevel)
         features.append(feature)
+
     return features
 
-
-def split_behavior(dataSeries, significanceLevel=0.01):
+def split_behavior(dataSeries, slope, curvature, significanceLevel=0.01):
     '''
-    Splits the given dataSeries into sections of different atomic behavior modes, and returns the array of these sections. Each element in 
+    Splits the given dataSeries into sections of different atomic behavior modes. Each element in 
     this 2-D array represents a section along the time-series that can be characterized as an atomic behaviour mode.
+
+    dataSeries: 1D np array; features1
+    slope: 1D np array; slope1 (slope of features1)
+    curvature: 1D np array; curvature1 (curvature of features1)
+
+    return: 2D np array; [[signSlopeSection1, signCurvatureSection1], [signSlopeSection2, signCurvatureSection2], ...
     '''
+
+    abs_data = np.abs(dataSeries)
+    abs_slope = np.abs(slope)
+    abs_curvature = np.abs(curvature)
     
-#     regr = linear_model.LinearRegression()
-# 
-#     # Train the model using the training sets
-#     regr.fit(np.asanyarray(range(dataSeries.shape[0])).reshape(-1,1), np.asanyarray(dataSeries).reshape(-1,1))
-#     predicted_series = regr.predict(np.asanyarray(range(dataSeries.shape[0])).reshape(-1,1))
-#     
-#     np.transpose(predicted_series).tolist()[0]
-    
-    slope = np.gradient(dataSeries)
-    curvature = np.gradient(slope)
-    
-    curvature[np.absolute(curvature)<np.absolute(slope)*significanceLevel]=0
-    slope[np.absolute(slope)<np.absolute(dataSeries)*significanceLevel]=0
-   
-                    
-    signSlope = slope.copy()
-    signSlope[signSlope>0] = 1
-    signSlope[signSlope<0] = -1
-    signSlope[signSlope==0] = 0
-    
-    signCurvature = curvature.copy()
-    signCurvature[signCurvature>0] = 1
-    signCurvature[signCurvature<0] = -1
-    signCurvature[signCurvature==0] = 0         
-    
-    sections = 10*signSlope+signCurvature
-    raw_input()
-    temp = sections[1::]-sections[0:sections.shape[0]-1]
-    transPoints = np.nonzero(temp)
-    numberOfSections = len(transPoints[0])+1
-    
-    featureVector = np.zeros(shape=(2,numberOfSections))
-    
-    for k in transPoints:
-        featureVector[0][0:len(featureVector[0])-1] = signSlope[k]
-        featureVector[1][0:len(featureVector[0])-1] = signCurvature[k]
-    featureVector[0][numberOfSections-1]= signSlope[-1]
-    featureVector[1][numberOfSections-1]= signCurvature[-1]
- 
-    # featureVector=extend_mids(featureVector)
-    # featureVector=extend_ends(featureVector)
-    
+    data_threshold = abs_data * significanceLevel
+    slope_threshold = abs_slope * significanceLevel
+
+    slope = slope * (abs_slope >= data_threshold)
+    curvature = curvature * (abs_curvature >= slope_threshold)
+
+    signSlope = np.sign(slope)
+    signCurvature = np.sign(curvature)
+
+    sections = signSlope * 10 + signCurvature
+
+    transitions = np.diff(sections)
+    transPoints = np.flatnonzero(transitions)
+    numberOfSections = len(transPoints) + 1
+
+    featureVector = np.empty((2, numberOfSections))
+
+    if numberOfSections == 1:
+        featureVector[0, 0] = signSlope[0]
+        featureVector[1, 0] = signCurvature[0]
+    else:
+        section_starts = np.concatenate(([0], transPoints + 1))
+        featureVector[0] = signSlope[section_starts]
+        featureVector[1] = signCurvature[section_starts]
+
     return featureVector
-
-
-
-
-def filter_series(series, parentSeries, thold):
-    '''
-    Filters out a given time-series for insignificant fluctuations. For 
-    example very small fluctuations due to numeric error of the simulator).
-    
-    Not used anymore
-    '''
-     
-    absParent = np.absolute(parentSeries[0:parentSeries.shape[0]-1])
-    absSeries = np.absolute(series)
-    cond = absSeries < thold
-    cond1a = np.not_equal(absParent, 0)
-    cond1b = absSeries < thold*absParent
-    cond2a = np.logical_not(cond1b)
-    cond2b = absSeries < thold/10
-    cond1 = np.logical_and(cond1a,cond1b)
-    cond2 = np.logical_and(cond2a,cond2b)
-    #cond = np.logical_or(cond1,cond2)
-    series[cond] = 0
-    return series
-
-
-def extend_mids(vector):
-    '''
-    Not used anymore 15/12/14
-    '''
-    sections = vector[0].size
-    added = 0
-    for i in range(sections-1):
-        if(vector[0][i+added]*vector[0][i+1+added]==-1) and\
-          (vector[0][i+added+1]!=0):
-            vector = np.insert(vector, i+1+added, 0, axis=1)
-            vector[1][i+1+added] = vector[1][i+added]
-            added+=1
-    return vector
-
-def extend_ends(vector):
-    '''
-    Not used anymore 15/12/14
-    '''
-    sections = vector[0].size
-    added = 0
-    
-    if(vector[0][0]*vector[1][0]==1):
-        #Front extension
-        vector = np.insert(vector, 0, 0, axis=1)
-        vector[1][0] = vector[1][1]
-        added+=1
-    if(vector[0][sections-1+added]*vector[1][sections-1+added]==-1):
-        #End extension
-        vector = np.append(vector, [[0],[0]], axis=1)
-        vector[1][sections+added] = vector[1][sections-1+added]
-        added+=1
-    return vector
-
-
-
 
 '''
 The main method where the user needs to specify the path to the simulation results
-'''   
+'''
 if __name__ == '__main__':
-    print 'gonenc'
+    print('gonenc')
--- src/distance_triangle.py
+++ src/distance_triangle.py
@@ -1,21 +1,11 @@
 '''
-Created on Nov 24, 2011
+Created on Dec 19, 2011
+@author: gyucel
 
-@author: gonengyucel
+Updated July 25, 2025
 '''
 import numpy as np
-
-from expWorkbench import EMAError
-from expWorkbench.EMAlogging import info
-
-def trdist(d1,d2):
-    
-    a = np.sum(d1*d2)
-    b = np.sqrt(np.sum(d1*d1))
-    c = np.sqrt(np.sum(d2*d2))
-    d = a/(b*c)
-    print d
-    return d
+from scipy.spatial.distance import pdist
 
 def distance_triangle(data):
     '''
@@ -23,41 +13,29 @@ def distance_triangle(data):
         Let ds1(.) and ds2(.) be two data series of length N. Then;
         A equals to the summation of ds1(i).ds2(i) from i=1 to N
         B equals to the square-root of the (summation ds1(i)^2 from i=1 to N)
-        C equals to the square-root of the (summation ds1(i)^2 from i=1 to N)
+        C equals to the square-root of the (summation ds2(i)^2 from i=1 to N)
         
         distance_triangle = A/(B.C)
-     
+
      The triangle distance works only with data series of the same length
      
      In the literature, it is claimed that the triangle distance can deal with noise and amplitude scaling very well, and may yield poor
-     results in cases of offset translation and linear drift.   
+     results in cases of offset translation and linear drift.
+
+    data: 2D array or np array; [[features1], [features2], ...]   ;;  Without labels
+    return: 2D np array; [distance_vector, [({'Index': 0}, features1), ({'Index': 1}, features2), ...]]
     '''
-    
-    
-    
-    runLogs = []
-    #Generates the feature vectors for all the time series that are contained in numpy array data
-    info("calculating distances")
-    dRow = np.zeros(shape=(np.sum(np.arange(data.shape[0])), ))
-    index = -1
-    for i in range(data.shape[0]):
-            
-        # For each run, a log is created
-        # Log includes a description dictionary that has key information 
-        # for post-clustering analysis, and the data series itself. These 
-        # logs are stored in a global array named runLogs
-        behaviorDesc = {}
-        behaviorDesc['Index'] = str(i)
-        
-        behavior = data[i]
-        localLog = (behaviorDesc, behavior)
-        runLogs.append(localLog)
-    
-        for j in range(i+1, data.shape[0]):
-            index += 1
-            distance = trdist(data[i],data[j]) 
-            dRow[index] = distance
+
+    if not isinstance(data, np.ndarray):
+        data = np.array(data)
+
+    runLogs = [({'Index': str(i)}, data[i]) for i in range(len(data))]
+
+    cosine_distances = pdist(data, metric='cosine')
+    dRow = 1 - cosine_distances
+
     return dRow, runLogs
 
 if __name__ == '__main__':
-    tester = np.array([(1,2,3,4,5,6,7,8,12,15),(11,22,32,32,26,24,23,16,12,10)])
+    tester = np.array([(12,4,1),(2,2,6), (1.5,1,1)])
+    #print(distance_triangle(tester))
